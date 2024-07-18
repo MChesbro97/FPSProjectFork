f8f5ec504f8693e4a16c762cb8e32c0b6b67eab2
MChesbro97 <mitchell.chesbro@hotmail.com>
2024-07-17 20:52:12 -0400

    July 17
    Initial commit for FPsProject in VGP221

---
+++ Source/VGP221FPSProject/Private/Player/FPSCharacter.cpp
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/FPSCharacter.h"
+
+// Sets default values
+AFPSCharacter::AFPSCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	check(FPSCameraComponent != nullptr);
+
+	FPSCameraComponent->SetupAttachment(CastChecked<USceneComponent, UCapsuleComponent>(GetCapsuleComponent()));
+	FPSCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f + BaseEyeHeight));
+	FPSCameraComponent->bUsePawnControlRotation = true;
+
+	if (!FPSMesh) {
+		FPSMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+		FPSMesh->SetupAttachment(FPSCameraComponent);
+		FPSMesh->bCastDynamicShadow = false;
+		FPSMesh->CastShadow = false;
+	}
+
+	GetMesh()->SetOwnerNoSee(true);
+}
+
+// Called when the game starts or when spawned
+void AFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	check(GEngine != nullptr);
+
+
+
+	// Display a debug message for five seconds. 
+
+	// The -1 "Key" value argument prevents the message from being updated or refreshed.
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("We are using FPSCharacter."));
+}
+
+// Called every frame
+void AFPSCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AFPSCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AFPSCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("LookHorizontal", this, &AFPSCharacter::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookVertical", this, &AFPSCharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AFPSCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AFPSCharacter::EndJump);
+	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFPSCharacter::Fire);
+}
+
+void AFPSCharacter::MoveForward(float value)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(Direction, value);
+
+	UE_LOG(LogTemp, Warning, TEXT("Value: %f"), value);
+}
+
+void AFPSCharacter::MoveRight(float value)
+{
+	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(Direction, value);
+
+	UE_LOG(LogTemp, Warning, TEXT("Value: %f"), value);
+}
+
+void AFPSCharacter::StartJump()
+{
+	bPressedJump = true;
+}
+
+void AFPSCharacter::EndJump()
+{
+	bPressedJump = false;
+}
+
+void AFPSCharacter::Fire()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Shooting"));
+}
+
+
+
---
+++ Source/VGP221FPSProject/VGP221FPSProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VGP221FPSProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VGP221FPSProject, "VGP221FPSProject" );
